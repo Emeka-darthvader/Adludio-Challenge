55d450efa47696b99496991a9495c77c29400558
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2019-12-08 23:17:04 +0000

    images

--- src/index.js
+++ src/index.js
@@ -8,7 +8,4 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
