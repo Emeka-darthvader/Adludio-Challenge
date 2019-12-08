699ac24e7b514e19fb02ef2fbe704153199cbe3f
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2019-12-08 22:28:48 +0000

    Updates

---
+++ precache-manifest.4c5ebb14bb77742fa624d9e03a17e5a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c1cb95865ec0cf4b5aa604796d0a28",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/index.html"
+  },
+  {
+    "revision": "1c6690341e95ae16fd99",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "54049a9db99ff0101960",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/css/main.4e15d56d.chunk.css"
+  },
+  {
+    "revision": "1c6690341e95ae16fd99",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/js/2.d546d065.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/js/2.d546d065.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54049a9db99ff0101960",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/js/main.4fa16de6.chunk.js"
+  },
+  {
+    "revision": "0e3647e2ae7211378d90",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/js/runtime-main.cd553faa.js"
+  },
+  {
+    "revision": "ff629c02385f2f3a199b7e0b3065d077",
+    "url": "Emeka-darthvader.github.io/Adludio-Challenge/static/media/google_logo.ff629c02.png"
+  }
+]);
