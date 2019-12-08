9b067409b56976bd22d02163424d5a09628d6ae7
Emeka Onyebuchi <chybyke.emeka@gmail.com>
2019-12-08 22:59:13 +0000

    Updates

---
+++ precache-manifest.62fab0135f4f2d859a148ebd5260a4f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc6f3cf631497be567b236901f53ea0",
+    "url": "/Adludio-Challenge/index.html"
+  },
+  {
+    "revision": "fe7c2353fca1ae74015c",
+    "url": "/Adludio-Challenge/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0b767cdfa790e2ded55f",
+    "url": "/Adludio-Challenge/static/css/main.4e15d56d.chunk.css"
+  },
+  {
+    "revision": "fe7c2353fca1ae74015c",
+    "url": "/Adludio-Challenge/static/js/2.8f0eb7d5.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/Adludio-Challenge/static/js/2.8f0eb7d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b767cdfa790e2ded55f",
+    "url": "/Adludio-Challenge/static/js/main.5577a07a.chunk.js"
+  },
+  {
+    "revision": "f9175742d714009d32cc",
+    "url": "/Adludio-Challenge/static/js/runtime-main.955abbde.js"
+  },
+  {
+    "revision": "ff629c02385f2f3a199b7e0b3065d077",
+    "url": "/Adludio-Challenge/static/media/google_logo.ff629c02.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "Emeka-darthvader.github.io/Adludio-Challenge/public/precache-manifest.6408e96c8d3b414dabe468bf54d6270f.js"
+  "/Adludio-Challenge/precache-manifest.62fab0135f4f2d859a148ebd5260a4f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("Emeka-darthvader.github.io/Adludio-Challenge/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Adludio-Challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
